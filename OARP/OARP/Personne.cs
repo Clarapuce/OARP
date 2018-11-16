7baf68960d3c63c78c1058038e984c3db9fa4ef0
Clara Sorita <claraso@hotmail.fr>
2018-11-16 16:09:13 +0100

    TUTO DES BLEUS POUR LES POO

---
+++ OARP/OARP/Personne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OARP
+{
+    class Personne
+    {
+        //PROPRIETE
+        public string Nom { get; set; }
+        public string Message { get; set; }
+
+
+        //CONSTRUCTEUR
+        public Personne(string nom)
+        {
+            Nom = nom;
+            Message = "Je suis une chèvre";
+        }
+
+        //METHODES
+        public void Parler()
+        {
+            Console.WriteLine( "Je parle !");
+        }
+    }
+}
---
+++ OARP/OARP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne Jeremie = new OARP.Personne("Jeremie"); //Utilise constructeur
+            Console.WriteLine(Jeremie.Message);
+            Jeremie.Message = "Je suis une punaise";
+            Console.WriteLine(Jeremie.Message);
+            Jeremie.Parler();
+        }
+    }
+}
