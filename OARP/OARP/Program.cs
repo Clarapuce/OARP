c9b88f6e6db69c3abd813f81e1e38f6113ff8bed
Clara Sorita <claraso@hotmail.fr>
2018-11-20 15:51:43 +0100

    Ajout de la récursivité à débugger

--- OARP/OARP/Program.cs
+++ OARP/OARP/Program.cs
@@ -13,16 +13,23 @@ namespace OARP
             Personne e1 = new Personne("1");
             Personne e2 = new Personne("2");
             Personne e3 = new Personne("3");
+            Personne vide=new Personne("");
 
             List<Personne> eleves = new List<Personne>();
-            eleves.Add(e1); eleves.Add(e2); eleves.Add(e3);
+            eleves.Add(vide);eleves.Add(e1); eleves.Add(e2); eleves.Add(e3);
 
             List<string> projets = new List<string>();
             projets.Add("A"); projets.Add("B"); projets.Add("C"); projets.Add("D");
-            Repartition R = new Repartition(projets,eleves);
+            Repartition R = new Repartition(projets,eleves,1);
+            e1.Projets.Add("A"); e1.Projets.Add("B");
+            e1.Voeux.Add(4);e1.Voeux.Add(3);
 
+            e2.Projets.Add("A"); e2.Projets.Add("C");
+            e2.Voeux.Add(4); e2.Voeux.Add(3);
 
-            R.Voter();
+            e3.Projets.Add("A"); e3.Projets.Add("C"); e3.Projets.Add("B"); e3.Projets.Add("D");
+            e3.Voeux.Add(4); e3.Voeux.Add(4); e3.Voeux.Add(4); e3.Voeux.Add(3);
+            R.Repartir();
         }
     }
 }
--- OARP/OARP/Repartition.cs
+++ OARP/OARP/Repartition.cs
@@ -10,11 +10,15 @@ namespace OARP
     {
         public List<string> Projets { get; set; }
         public List<Personne> Eleves { get; set; }
+        public int NbPlaces{ get; set; }
+        public List<Personne> Combinaison { get; set; }
 
-        public Repartition(List<string> p, List<Personne> e)
+        public Repartition(List<string> p, List<Personne> e,int nbplace)
         {
             Projets = p;
             Eleves = e;
+            NbPlaces=nbplace;
+            Combinaison = new List<Personne>();
         }
         
         public void Voter()
@@ -42,13 +46,67 @@ namespace OARP
 
         public void Repartir()
         {
-            List<Personne> combinaison = new List<Personne>(Eleves);
+            //On met un élève vide dans chaque espace que devra prendre un elève dans la combinaison
+            //ça permet de voir si un elève a déjà été mis à la place en question 
+            for(int i = 0; i<Projets.Count();i++)
+            {
+                Combinaison.Add(Eleves[0]);
+            }
+            CalculerNote();
+            TesterCombinaison(Eleves[1]);           
+        }
+
+        public void ReinitialiserCombinaison()
+        //Fonction permettant, quand on a fini une combinaison, de remettre un espace vide à chaque espace
+        {
+            for(int i = 0; i<Projets.Count();i++)
+            {
+                Combinaison[i]=Eleves[0];
+            }
             
         }
-        
-        public void CalculerNote(List<Personne> configuration)
-        {
 
+        public void TesterCombinaison(Personne eleve)
+        //Fonction recursive permettant de tester toutes les possibilités
+        {
+            int index = Eleves.IndexOf(eleve);
+            foreach(string p in eleve.Projets)
+            {
+                int indexP= Projets.IndexOf(p);
+                if(Combinaison[indexP]==Eleves[0])
+                {
+                    Combinaison[indexP]=eleve;
+                }
+
+                //Condition d'arret de la recursivité : Si on a plus d'élèves à traiter en stock
+                if(index+1>=Eleves.Count())
+                {
+                    CalculerNote();
+                    ReinitialiserCombinaison();
+                    Console.WriteLine("===================");
+                }
+                else
+                {
+                    TesterCombinaison(Eleves[index + 1]);
+                }
+                
+
+            }
+        }
+        
+        public void CalculerNote()
+        {
+            AfficherCombinaison();
+        }
+
+        public void AfficherCombinaison()
+        {
+            string affichage = "";
+            for(int i =0; i<Combinaison.Count();i++)
+            {
+                affichage += "Place " + i+" : "+Combinaison[i].Nom + "\n";
+            }
+            Console.WriteLine(affichage);
         }
     }
 }
